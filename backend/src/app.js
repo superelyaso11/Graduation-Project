aeb0697f63c1a72e85ee24a7d3b39d9557d0632c
superelyaso11 <emaky13@gmail.com>
2026-04-20 20:01:07 +0300

    fix: open cors for production in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require('./routes/auth.routes')
-const LostItemRoutes = require('./routes/lostItem.routes')
-const FoundItemRoutes = require('./routes/foundItem.routes')
-const ClaimRoutes = require('./routes/claim.routes')
-const app = express()
+const authRoutes = require('./routes/auth.routes');
+const LostItemRoutes = require('./routes/lostItem.routes');
+const FoundItemRoutes = require('./routes/foundItem.routes');
+const ClaimRoutes = require('./routes/claim.routes');
+const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }))
-app.use(express.json())
-app.use('/api/auth', authRoutes)
-app.use('/api/lost-items', LostItemRoutes)
-app.use('/api/found-items', FoundItemRoutes)
-app.use('/api/claims', ClaimRoutes)
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/lost-items', LostItemRoutes);
+app.use('/api/found-items', FoundItemRoutes);
+app.use('/api/claims', ClaimRoutes);
 app.get('/', (req, res) => {
-    res.json({message: 'Lost and Found API is running'})
-})
+  res.json({ message: 'Lost and Found API is running' });
+});
 
-module.exports = app
+module.exports = app;
